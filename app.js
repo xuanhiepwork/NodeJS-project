8fc0582c005e0e9e6749f7b8d6a93117af305b3e
xuanhiep <xuanhiep.work@gmail.com>
2026-01-04 13:50:17 +0700

    pug template

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', expressHbs());
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
@@ -18,14 +20,16 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3200);
 
 //bài 65 || #4:30 | npm install --save body-parser
 //bài 82 || #0:30 | npm install --save ejs pug express-handlebars (tải 3 thư viện template engine)
-//bài 80 | Target ngày 3/1/2026
+//bài 88 || npm install --save express-handlebars@3.0
+//bài 96 | Target ngày 4/1/2026
 //bài 85 - 0:00
 
 //Nhớ đánh giá điểm rèn luyện trên trường không là tạch đấy =))
