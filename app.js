9b7a0979e53bad9835ec4577f567bb2387e29e18
xuanhiep <xuanhiep.work@gmail.com>
2026-01-08 11:34:11 +0700

    work with ejs

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-    'hbs',
-    expressHbs({
-        layoutsDir: 'views/layouts/',
-        defaultLayout: 'main-layout',
-        extname: 'hbs'
-    })
-);
-app.set('view engine', 'hbs');
+// app.engine(
+//     'hbs',
+//     expressHbs({
+//         layoutsDir: 'views/layouts/',
+//         defaultLayout: 'main-layout',
+//         extname: 'hbs'
+//     })
+// );
+// app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -27,7 +28,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 });
 
 app.listen(3200);
@@ -36,7 +37,5 @@ app.listen(3200);
 //bài 82 || #0:30 | npm install --save ejs pug express-handlebars (tải 3 thư viện template engine)
 //bài 88 || npm install --save express-handlebars@3.0
 //bài 96 | Target ngày 4/1/2026
-//bài 92 - 0:00
-
-
-//Nhớ đánh giá điểm rèn luyện trên trường không là tạch đấy =))
+//bài 93 - 0:00 | Coi lại xem lỗi gì
+//bài 94 - 0:00
