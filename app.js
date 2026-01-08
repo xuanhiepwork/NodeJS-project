c7687958a0e465d7e9b232aea3393d9ea8308d6c
xuanhiep <xuanhiep.work@gmail.com>
2026-01-08 11:48:18 +0700

    using controllers

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const expressHbs = require('express-handlebars');
 
+const errorController = require('./controllers/error');
+
+
 const app = express();
 
 // app.engine(
@@ -18,18 +21,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
-});
+app.use(errorController.get404);
 
 app.listen(3200);
 
@@ -37,5 +38,4 @@ app.listen(3200);
 //bài 82 || #0:30 | npm install --save ejs pug express-handlebars (tải 3 thư viện template engine)
 //bài 88 || npm install --save express-handlebars@3.0
 //bài 96 | Target ngày 4/1/2026
-//bài 93 - 0:00 | Coi lại xem lỗi gì
-//bài 94 - 0:00
+//bài 95 - 0:00
